62c11f8615ac91949e2d62a8bf54c27537e3c94f
Tyler Reed <tylernathanreed@gmail.com>
2019-03-02 12:43:18 -0600

    + Added basis for testing

--- nova-components/SidebarTool/webpack.mix.js
+++ nova-components/SidebarTool/webpack.mix.js
@@ -1,4 +1,10 @@
 let mix = require('laravel-mix')
 
-mix.js('resources/js/tool.js', 'dist/js')
-   .sass('resources/sass/tool.scss', 'dist/css')
+mix.setPublicPath('dist')
+	.js('resources/js/tool.js', 'dist/js')
+	.sass('resources/sass/tool.scss', 'dist/css')
+	.webpackConfig({
+		resolve: {
+			symlinks: false
+		}
+	})
